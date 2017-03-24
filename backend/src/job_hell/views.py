629a6bbbaa81a2948ce9526d92bfdbc541016484
vasu-dev <vasu.singh002@gmail.com>
2017-03-24 20:03:06 +0530

    Backend and ML model added

---
+++ backend/src/job_hell/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .utlis.nn import predict 
+# Create your views here.
+
+@csrf_exempt
+def get_score(request):
+	if request.method == 'POST':
+		data = request.POST['data']
+		score = predict(data)
+		return JsonResponse({'score':score})
+	else:
+		return render(request, "main/thing.html")
+		return JsonResponse({'Error':'No POST request'})
